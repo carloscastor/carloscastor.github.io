59bba22c5c33652a3fee5faa1b418052c302011b
Carlos Castor <carlos.castor@gmail.com>
2020-03-05 08:18:57 -0300

    Updates

---
+++ precache-manifest.8ef46546be0b56adde429398247d3a32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6914046b05056cfa4e3b74bc1fbf93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "deac8bf5556590d92c75",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e8c8cb1f0d05467a78cb",
+    "url": "/static/js/2.cadf1f5b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.cadf1f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deac8bf5556590d92c75",
+    "url": "/static/js/main.d0f5642d.chunk.js"
+  },
+  {
+    "revision": "6da6f7b7a260998c5362",
+    "url": "/static/js/runtime-main.edf2f41f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
